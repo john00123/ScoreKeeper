0d9d9c0b359f6e54297c0a3266a957fb79b0426f
john <john.torres.hurtado@gmail.com>
2018-04-27 05:47:53 -0700

    last space
    last blank space

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    
+
     public void displayForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
